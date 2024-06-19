c525740be266293ec369b235e953027212ef6db5
YuwanM20 <anyougitr@gmail.com>
2024-06-19 15:50:13 +0700

    Pembaruan Link API (Keamanan)

--- routes/api.php
+++ routes/api.php
@@ -1,55 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Response;
-use App\Http\Controllers\Api\LahanApi;
-use App\Http\Controllers\Api\AkunApi;
-use App\Http\Controllers\Api\PereApi;
-use App\Http\Controllers\Api\KopiApi;
-use App\Http\Controllers\Api\HargaApi;
-
-// Lahan
-Route::get('/lahan/tampil', [LahanApi::class, 'index']);
-Route::get('/lahan/tampil/{kode_lahan}', [LahanApi::class, 'show']);
-Route::get('/lahan/tampil/user/{user}', [LahanApi::class, 'showuser']);
-Route::post('/lahan/tambah', [LahanApi::class, 'store']);
-Route::put('/lahan/update/{kode_lahan}', [LahanApi::class, 'updatee']);
-Route::delete('/lahan/hapus/{kode_lahan}', [LahanApi::class, 'destroy']);
-
-// Akun
-Route::get('/akun/tampil', [AkunApi::class, 'index']);
-Route::get('/akun/tampil/{user}', [AkunApi::class, 'showuser']);
-Route::get('/akun/tampil/email/{email}', [AkunApi::class, 'showemail']);
-Route::post('/akun/tambah', [AkunApi::class, 'store']);
-Route::put('/akun/update/{user}', [AkunApi::class, 'update']);
-Route::put('/akun/update/email/{email}', [AkunApi::class, 'updatee']);
-Route::delete('/akun/hapus/{user}', [AkunApi::class, 'delete']);
-
-// Peremajaan
-Route::get('/pere/tampil', [PereApi::class, 'index']);
-Route::get('/pere/tampil/{kode_peremajaan}', [PereApi::class, 'show']);
-Route::get('/pere/tampil/lahan/{kode_lahan}', [PereApi::class, 'showperelahan']);
-Route::get('/pere/tampil/user/{user}', [PereApi::class, 'showuserpere']);
-Route::post('/pere/tambah', [PereApi::class, 'store']);
-Route::put('/pere/update/{kode_lahan}', [PereApi::class, 'updatee']);
-Route::delete('/pere/hapus/{kode_peremajaan}', [PereApi::class, 'destroy']);
-// Route::delete('/pere/hapus/lahan/{kode_lahan}', [PereApi::class, 'hapuslahan']);
-
-// Kopi
-Route::get('/kopi/tampil', [KopiApi::class, 'index']);
-Route::get('/kopi/tampil/pere/{kode_peremajaan}', [KopiApi::class, 'showpere']);
-Route::get('/kopi/tampil/user/{user}', [KopiApi::class, 'showuser']);
-Route::get('/kopi/tampil/{kode_kopi}', [KopiApi::class, 'show']);
-Route::post('/kopi/tambah', [KopiApi::class, 'store']);
-Route::put('/kopi/update/{user}', [KopiApi::class, 'update']);
-Route::put('/kopi/update/email/{email}', [KopiApi::class, 'updatee']);
-Route::delete('/kopi/hapus/{kode_kopi}', [KopiApi::class, 'delete']);
-
-// Harga Kopi
-Route::get('/hargakopi/tampil', [HargaApi::class, 'index']);
-Route::get('/hargakopi/{id}', [HargaApi::class, 'showid']);
-
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
